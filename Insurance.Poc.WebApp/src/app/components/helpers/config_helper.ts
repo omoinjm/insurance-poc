e1ba91952dc5bafa681acb1b345904f2455ac4ba
omoinjm <njmcloud@gmail.com>
2024-12-29 22:17:21 +0200

    add Dockerfile x2

--- Insurance.Poc.WebApp/src/app/components/helpers/config_helper.ts
+++ Insurance.Poc.WebApp/src/app/components/helpers/config_helper.ts
@@ -1,6 +1,7 @@
 export class ConfigHelper {
-
+   /**
+    * Base URL for API requests.
+    * This value is retrieved from environment variables or defaults to an empty string.
+    */
    public static API_URL: string = process.env["API_URL"] || '';
-
-
 }
